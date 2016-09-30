ff5816c4b1957fb2fe1e4a8f7807eac029691600
Karl <karl@xsact.com>
2016-10-01 01:40:35 +0200

    initial commit

---
+++ demo/demo-api.php
@@ -0,0 +1,128 @@
+<?php
+/*
+This is a simple demo API showing how to interact with the
+transact-io.php library
+
+You are free to use this example in any way you wish.
+If you feel this exmaple can be improved please seed
+a git pull request
+*/
+
+require_once __DIR__ . '/../transact-io.php'; // Include lib
+
+// simple response object for errors
+class ErrorResponse {
+  public $code = 400;
+  public $messsage = '';
+  public $data = null;
+  function __construct($code, $msg, $data=null) {
+      $this->code = $code;
+      $this->message = $msg;
+      $this->data = $data;
+   }
+}
+
+// all of our responses are JSOn
+header('Content-Type: text/javascript; charset=utf8');
+
+$transact = new TransactIoMsg();
+
+// Required: set the secret use to sign messages
+// The secret needs to be set for outboud message tokens
+// and it also needs to be set for inbound validation
+$transact->setSecret('Signing Secret');
+
+// Optional: default signing algorithim
+$transact->setAlg('HS256');
+
+function InitSaleParameters($transact) {
+
+  // Required: set ID of who gets paid
+  $transact->setRecipient('5206507264147456');
+
+  // Required:  Set the price of the sale
+  $transact->setPrice(2);
+
+  // Required:  Set PROD to use real money,  TEST for testing
+  $transact->setClass('PROD');
+
+  // Required:  set URL associated with this puchase
+  // User should be able to return to this URL
+  $transact->setURL('https://example.site/article1/');
+
+  // Recommended: Title for customer to read for the purchase
+  $transact->setTitle('PHP Demo Title');
+
+  $transact->setMethod('CLOSE'); // Optional: by default close the popup
+
+
+  // Unique code for seller to set to what they want
+  //  This could be a code for the item your selling
+  $transact->setItem('ItemCode1');
+
+  // Optional Unique ID of this sale
+  $transact->setUid('ItemCode1');
+
+  // Set your own meta data
+  // Note you must keep this short to avoid going over the 1024 byte limt
+  // of the token URL
+  $transact->setMeta(array(
+    'your' => 'data',
+    'anything' => 'you want'
+  ));
+
+  return $transact;
+}
+
+function fetchPremiumContent($token) {
+
+}
+
+switch($_REQUEST['action']) {
+
+  case 'getToken':
+
+    $transact = InitSaleParameters($transact);
+
+    $response = array(
+      token => $transact->getToken()
+    );
+    echo json_encode($response);
+
+ //
+      /*
+
+*/
+
+    break;
+
+  case 'getPurchasedContent':
+
+    try {
+      $decoded = $transact->decodeToken($_REQUEST['t']);
+      echo json_encode(array(
+        'content' => 'SUCESSS PAID CONTENT HERE!',
+        'status' => 'OK',
+        'decoded' => $decoded
+        ));
+     } catch (Exception $e) {
+
+       echo json_encode(array(
+        'content' => 'Failed validation',
+        'status' => 'ERROR',
+        'message' =>  $e->getMessage(),
+        ));
+     }
+
+  break;
+  default:
+    header("HTTP/1.0 404 Not Found");
+
+    echo json_encode(new ErrorResponse('404', 'Invalid API call'));
+    exit;
+};
+
+
+
+
+?>
---
+++ transact-io.php
@@ -0,0 +1,110 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+
+session_set_cookie_params(86400*365,"/");
+session_start();
+
+
+class TransactIoMsg {
+
+  private $secret = 'Signing Secret'; // signing secret. CHANGE this to YOURS
+
+  private $token = array(
+
+    'iat' => 0, // issued at
+    'item' => '',   //code for what they are buying
+    'method' => 'CLOSE',  // CLOSE (popup) or POST (to page)
+    'price' => 0, // price in cents
+    'recipient' => '', // recipient who recieves the funds
+    'tclass' => 'TEST', //Class or Currency to use.  TEST or PROD,
+    'title' => 'Description for Humans to read', // describe
+    'uid' => '', // Unique ID to identify
+    'url' => '', // URL of what they are buying
+);
+
+  private $method='CLOSE';
+  private $alg='HS256';  // use the SHA 256 Algorithim
+
+  function __construct() {
+
+   }
+
+  function setSecret($val) {
+    $this->secret = $val;
+  }
+
+  function setRecipient($val) {
+    // this should be a 64 bit int,
+    // treat as string incase we are on a 32bit machine
+    $this->token['recipient'] =  (string) $val;
+  }
+  function setPrice($val) {
+    if ($val < 1 || $val > 1000) {
+      throw new Exception('Price must be betwen 1 and 1000');
+    }
+
+   $this->token['price'] = (int) $val;
+  }
+  function setClass($val) {
+    $this->token['tclass'] = $val;
+  }
+
+  function setMethod($val) {
+    switch($val) {
+      case 'POST':
+      case 'CLOSE':
+        $this->methhod = $val;
+        break;
+      default:
+        throw new Exception('method must be POST or CLOSE (popup)');
+    }
+  }
+
+  function setAlg($val) {
+     switch($val) {
+      case 'HS256':
+      case 'ES256':
+        $this->alg = $val;
+        break;
+      default:
+        throw new Exception('HS256 and ES256 supported');
+    }
+  }
+  function setTitle($val) {
+    $this->token['title'] = $val;
+  }
+  function setItem($val) {
+    $this->token['item'] = $val;
+  }
+  function setUid($val) {
+    $this->token['uid'] = $val;
+  }
+  function setMeta($val) {
+    $this->token['meta'] = $val;
+  }
+
+  function setURL($val) {
+    $this->token['url'] = $val;
+  }
+
+  function getToken() {
+    $this->token['iat'] = time();  // set timestamp
+
+    if (empty($this->secret))
+      throw new Exception('Must set signing secret');
+
+    $token = JWT::encode($this->token, $this->secret);
+    return $token;
+  }
+
+  function decodeToken($token) {
+    return JWT::decode($token, $this->secret, array($this->alg));
+  }
+
+}
+
+
+?>
